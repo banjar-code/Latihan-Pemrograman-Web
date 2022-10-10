99e2eebd977398036ad282fa8ad100154c0c091c
Panduan Code <50077727+PanduanCode@users.noreply.github.com>
2022-10-10 10:55:21 +0700

    Add files via upload

---
+++ latihan_if.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan IF 11210072</title>
+</head>
+<body>
+    <?php
+    $nama = "Arif Noer Wahidin";
+    $Nama = "Arif NoerWahidin";
+    echo "isi variable \$nama = $nama";
+    echo "<br>";
+    echo "isi variable \$Nama = $Nama";
+    ?>
+</body>
+</html>
